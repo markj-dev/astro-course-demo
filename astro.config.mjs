45c37f5ab1d04aa4774df0b95ae3a348283efb5c
markj-dev <105235719+markj-dev@users.noreply.github.com>
2025-01-07 12:44:16 -0600

    installed svelte, added contact form, connected to KwesForms

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from "astro/config"
 
 import tailwind from "@astrojs/tailwind"
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), svelte()],
   site: "http://rhythm.nation",
 })
---
+++ src/components/ContactForm.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import pkg from 'kwesforms';
+    const {kwesforms} = pkg;
+    // import {kwesforms} from "kwesforms"
+    import {onMount} from "svelte"
+
+    onMount(() => {
+        kwesforms.init()
+    })
+    
+</script>
+
+<form 
+    class="grid gap-y-2 kf-form items-end max-md:grid-cols-1 kwes-form" 
+    method="POST" 
+    action="https://kwesforms.com/api/f/vRH15xGakdYXfb6Vo7ii"
+    redirect="/thankyou"
+    no-error-message>
+    <label for="comment" class="text-zinc-900 text-2xl mb-2 mt-10">
+        What's on your mind?
+    </label>
+    <textarea 
+        rows="3" 
+        name="comment" 
+        id="comment" 
+        rules="required|max:255"
+        class="border border-teal-900 rounded-lg text-2xl px-6 py-6"></textarea>
+    <button 
+        type="submit"
+        class="text-2xl text-zinc-100 bg-teal-900 px-6 py-5 rounded-xl my-1">
+        Send
+    </button>
+</form>
