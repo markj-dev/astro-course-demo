f91bae3d18517f1476bac5394150928c5036d696
markj-dev <105235719+markj-dev@users.noreply.github.com>
2025-01-07 16:15:59 -0600

    Updated the site for app

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@ import { defineConfig } from "astro/config"
 
 import tailwind from "@astrojs/tailwind"
 
-import svelte from "@astrojs/svelte";
+import svelte from "@astrojs/svelte"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
-  site: "http://rhythm.nation",
+  site: "https://rhythmnationblog.netlify.app",
 })
