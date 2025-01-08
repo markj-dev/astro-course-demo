56a076882d23e2a85cdb4b55ead4024d9325be8c
markj-dev <105235719+markj-dev@users.noreply.github.com>
2025-01-08 16:57:25 -0600

    converted to Server-side Rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,11 @@ import tailwind from "@astrojs/tailwind"
 
 import svelte from "@astrojs/svelte"
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   site: "https://rhythmnationblog.netlify.app",
+  adapter: netlify(),
 })
