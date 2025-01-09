fda452c6169d69818359cfe48420e393ed9ffd0c
markj-dev <105235719+markj-dev@users.noreply.github.com>
2025-01-09 14:55:21 -0600

    add author and cloudinary

---
+++ src/cloudinary.ts
@@ -0,0 +1,23 @@
+import { Cloudinary } from "@cloudinary/url-gen"
+import { source } from "@cloudinary/url-gen/actions/overlay"
+import { text } from "@cloudinary/url-gen/qualifiers/source"
+import { TextStyle } from "@cloudinary/url-gen/qualifiers/textStyle"
+
+const cloudinary = new Cloudinary({
+  cloud: {
+    cloudName: import.meta.env.CLOUDINARY_CLOUD_NAME,
+  },
+  url: {
+    secure: true,
+  },
+})
+
+const getThumbnail = (title: string) => {
+  const thumbnail = cloudinary
+    .image("astro-course-template")
+    .overlay(source(text(title, new TextStyle("Cabin", 64).fontWeight("bold"))))
+
+  return thumbnail.toURL()
+}
+
+export { cloudinary, getThumbnail }
